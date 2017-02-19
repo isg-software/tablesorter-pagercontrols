2c8ef3776325862b29cf8eb5e4044dd63102d271
Immo Schulz-Gerlach <immo@isg-software.de>
2017-02-19 21:42:00 +0100

    Second template: Unicode triange icons
    Button labels as already shown in an example page now available as option template as well.

--- src/js/jquery.tablesorter.pager.appendcontrols.js
+++ src/js/jquery.tablesorter.pager.appendcontrols.js
@@ -240,6 +240,17 @@
 		fullwidth: {
 			controlsOutput: '<div>{first}{prev}</div><div>{display} {size}</div><div>{next}{last}</div>',
 			classControls: "tablesorterPagerControls fullwidth"
+		},
+		/**
+		 * Alternative set of button labels using Unicode left or right pointing triangles.
+		 * Merge this object with you options object passed to the jQuery plug-in for a single call
+		 * or into $.fn.appendTablesorterPagerControls.defaults in order to make this the default template.
+		 */
+		triangleIcons: {
+			labelFirst: '|◀︎',
+			labelLast: '▶︎|',
+			labelPrev: '◀︎',
+			labelNext: '▶︎'
 		}
 	};
  
